463b46f7c174596f0d4c576c1aab49f474e2b745
simran-shankar <78287297+simran-shankar@users.noreply.github.com>
2023-07-10 09:42:07 +0530

    BayesianNetwork.py

--- ML OEE/07.BayesianNetwork.py
+++ ML OEE/07.BayesianNetwork.py
@@ -1,8 +1,4 @@
-"""
-7. Write a program to construct a Bayesian network considering medical data. Use this model to
-demonstrate the diagnosis of heart patients using standard Heart Disease Data Set. You can use
-Java/Python ML library classes/API.
-"""
+
 
 import pandas as pd
 from pgmpy.estimators import MaximumLikelihoodEstimator
